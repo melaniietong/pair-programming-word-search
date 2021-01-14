ef7783e700ff6ef56e80d518fddbd0f09f8ea84d
Melanie Tong <melaniietong@gmail.com>
2021-01-14 00:14:49 +0000

    Refactored

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -3,8 +3,8 @@ const assert = chai.assert;
 
 const wordSearch = require('../wordsearch.js')
 
-describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+describe("#wordSearch()", function () {
+  it("should return false if the word is not present", function () {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -20,7 +20,8 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present and horizontal", function () {
+
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -35,4 +36,63 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is in lowercase and is present and vertical", function () {
+
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'L', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'larry')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present and vertical", function () {
+
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'L', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'HELLO')
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if the array is empty", function () {
+
+    const result = wordSearch(
+      [], 'HELLO')
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if is one level deep", function () {
+
+    const result = wordSearch(
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'], 'HELLO')
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if some subarrays are arrays and some aren't", function () {
+
+    const result = wordSearch(
+      [[], 'A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'], 'HELLO')
+
+    assert.isFalse(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,21 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) return false;
+
+    for (row of letters) {
+        if (!Array.isArray(row)) return false;
+    }
+
+    const newWord = word.toUpperCase();
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(newWord)) return true;
     }
+    for (let i = 0; i < letters[0].length; i++) {
+        let column = letters.map(element => element[i]);
+        let columnJoined = column.join('');
+        if (columnJoined.includes(newWord)) return true;
+    }
+    return false;
 }
 
 module.exports = wordSearch
